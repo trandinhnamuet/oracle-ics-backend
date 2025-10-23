823114a9f8dc422db155b0850a497defaa23ffe3
Trần Đình Nam <trandinhnamuet@gmail.com>
2025-10-23 08:44:49 +0700

    check contrant before delete user package table

--- src/migrations/20251006100000-RemoveUserPackageUniqueConstraint.ts
+++ src/migrations/20251006100000-RemoveUserPackageUniqueConstraint.ts
@@ -2,8 +2,16 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class RemoveUserPackageUniqueConstraint20251006100000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropIndex("user_package", "IDX_USER_PACKAGE_UNIQUE");
-  }
+        const indexExists = await queryRunner.query(`
+            SELECT 1
+            FROM pg_indexes
+            WHERE tablename = 'user_package' AND indexname = 'IDX_USER_PACKAGE_UNIQUE'
+        `);
+
+        if (indexExists.length > 0) {
+            await queryRunner.dropIndex('user_package', 'IDX_USER_PACKAGE_UNIQUE');
+        }
+    }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createIndex(
