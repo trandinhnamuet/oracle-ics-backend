ff7b4a9155ebfdc477c6eb756e7fdce9466c3358
Trần Đình Nam <trandinhnamuet@gmail.com>
2025-11-11 15:27:43 +0700

    auto logout expire token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { User } from '../entities/user.entity';
     TypeOrmModule.forFeature([User]),
     PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'your-secret-key',
+      secret: process.env.JWT_SECRET || '***REMOVED-JWT***',
       signOptions: { expiresIn: '24h' },
     }),
   ],
